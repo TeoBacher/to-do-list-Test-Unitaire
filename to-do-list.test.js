697e837a687e8367caf57b9cc130f2ab21d1d988
w0lwin <teo.bacher@hotmail.com>
2024-10-24 18:38:58 +0200

    test: :test_tube: implement test save()

--- to-do-list.test.js
+++ to-do-list.test.js
@@ -1,1 +1,25 @@
 const todo = require("./to-do-list");
+const Item = require('./Item');
+const User = require('./user');
+
+
+beforeEach(() => {
+    const now = new Date(); 
+    now.setMinutes(now.getMinutes() - 29);
+    const item = new Item('Mi','zdekfrfjuhoiafkozjG' ,now);
+    user = new User(
+      "Loris",
+      "Laurenti",
+      "lloris@outlook.fr",
+      new Date(200, 1, 1),
+      "Password123"
+    );
+    todo = new ToDoList(item, user);
+  });
+
+test('should return false when save is unsuccessful', () => {
+    todo.save = jest.fn().mockReturnValue(true);
+
+    const result = todo.save();
+    expect(result).toBe(true);  
+  });
