3abc059836370aed604d25def01a09ea638a344d
redes19 <120016236+redes19@users.noreply.github.com>
2024-10-25 16:57:15 +0200

    delete

--- email.js
+++ email.js
@@ -4,38 +4,6 @@ class EmailSenderService {
     this.ownerMessage = O;
     this.receiverMessage = R;
   }
-
-  setMessage(M) {
-    this.message = M;
-  }
-
-  setOwnerMessage(O) {
-    this.ownerMessage = O;
-  }
-
-  setReceiverMessage(R) {
-    this.receiverMessage = R;
-  }
-
-  mailIsValid() {
-    return (
-      this.messageIsValid() &&
-      this.ownerMessageIsValid() &&
-      this.receiverMessageIsValid()
-    );
-  }
-
-  messageIsValid() {
-    return this.message.length > 0;
-  }
-
-  ownerMessageIsValid() {
-    return this.ownerMessage.length > 0;
-  }
-
-  receiverMessageIsValid() {
-    return this.receiverMessage.length > 0;
-  }
 }
 
 module.exports = EmailSenderService;
